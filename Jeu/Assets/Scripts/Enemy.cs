750b36d115a267e407a09c5d3f73dda62219ae18
Wastchen <davenduval48@gmail.com>
2020-12-13 20:52:59 -0500

    Trying

---
+++ Jeu/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_enemyTransform = null;
+    [SerializeField]
+    private SpriteRenderer m_enemySprite = null;
+    public int moveSpeed = 3;
+    public bool goingRight = false;
+    public bool goingLeft = true;
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (m_enemyTransform.position.x > 0 && goingLeft == true)
+        {
+            m_enemyTransform.position = m_enemyTransform.position += (transform.right * -moveSpeed * Time.deltaTime);
+            m_enemySprite.flipX = false;
+        }
+        else
+        {
+            goingLeft = false;
+            goingRight = true;
+        }
+
+        if (m_enemyTransform.position.x < 8 && goingRight == true)
+        {
+            m_enemyTransform.position = m_enemyTransform.position += (transform.right * moveSpeed * Time.deltaTime);
+            m_enemySprite.flipX = true;
+        }
+        else
+        {
+            goingLeft = true;
+            goingRight = false;
+        }
+    }
+}
---
+++ Jeu/Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_playerTransform = null;
+    [SerializeField]
+    private SpriteRenderer m_playerSprite = null;
+    [SerializeField]
+    private Animator m_playerAnimator = null;
+    [SerializeField]
+    private Rigidbody2D m_playerRigidbody = null;
+
+    public int moveSpeed = 3;
+    public int jumpHeight = 30;
+    private bool canJump = true;
+
+    void Update()
+    {
+        Move();
+        Jump();
+        Attack();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_playerTransform.position = m_playerTransform.position += (transform.right * -moveSpeed * Time.deltaTime);
+            m_playerSprite.flipX = true;
+            m_playerAnimator.SetBool("isRunning", true);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                m_playerAnimator.SetBool("isSliding", true);
+            }
+            else
+            {
+                m_playerAnimator.SetBool("isSliding", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            m_playerTransform.position = m_playerTransform.position += (transform.right * moveSpeed * Time.deltaTime);
+            m_playerSprite.flipX = false;
+            m_playerAnimator.SetBool("isRunning", true);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                m_playerAnimator.SetBool("isSliding", true);
+            }
+            else
+            {
+                m_playerAnimator.SetBool("isSliding", false);
+            }
+        }
+        else
+        {
+            m_playerAnimator.SetBool("isRunning", false);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+            m_playerRigidbody.AddForce(Vector2.up * jumpHeight);
+            m_playerAnimator.SetBool("isJumping", true);
+            canJump = false;
+        }
+        else
+        {
+            m_playerAnimator.SetBool("isJumping", false);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            m_playerAnimator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            m_playerAnimator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+}
