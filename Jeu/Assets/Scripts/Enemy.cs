a6e7e22f5d057dbf4b7578335f428511df1f4d58
Wastchen <davenduval48@gmail.com>
2021-01-21 21:05:52 -0500

    Hp Hp Hud Anim Character damage Anim Enemy Damage Anim Enemy Death Hitbox for sword Slight fix to some anim

--- Jeu/Assets/Scripts/Enemy.cs
+++ Jeu/Assets/Scripts/Enemy.cs
@@ -8,36 +8,66 @@ public class Enemy : MonoBehaviour
     private Transform m_enemyTransform = null;
     [SerializeField]
     private SpriteRenderer m_enemySprite = null;
+    [SerializeField]
+    private Animator m_enemyAnimator = null;
     public int moveSpeed = 3;
+    private float deathTimer = 1f;
     public bool goingRight = false;
     public bool goingLeft = true;
+    private bool wasHit = false;
     void Update()
     {
         Move();
+        Death();
     }
 
     private void Move()
     {
-        if (m_enemyTransform.position.x > 0 && goingLeft == true)
+        if (wasHit == false)
         {
-            m_enemyTransform.position = m_enemyTransform.position += (transform.right * -moveSpeed * Time.deltaTime);
-            m_enemySprite.flipX = false;
-        }
-        else
-        {
-            goingLeft = false;
-            goingRight = true;
-        }
+            if (m_enemyTransform.position.x > 0 && goingLeft == true)
+            {
+                m_enemyTransform.position = m_enemyTransform.position += (transform.right * -moveSpeed * Time.deltaTime);
+                m_enemySprite.flipX = false;
+            }
+            else
+            {
+                goingLeft = false;
+                goingRight = true;
+            }
 
-        if (m_enemyTransform.position.x < 8 && goingRight == true)
-        {
-            m_enemyTransform.position = m_enemyTransform.position += (transform.right * moveSpeed * Time.deltaTime);
-            m_enemySprite.flipX = true;
+            if (m_enemyTransform.position.x < 8 && goingRight == true)
+            {
+                m_enemyTransform.position = m_enemyTransform.position += (transform.right * moveSpeed * Time.deltaTime);
+                m_enemySprite.flipX = true;
+            }
+            else
+            {
+                goingLeft = true;
+                goingRight = false;
+            }
         }
-        else
+    }
+
+    private void Death()
+    {
+        if (wasHit)
         {
-            goingLeft = true;
-            goingRight = false;
+            deathTimer -= Time.deltaTime;
+
+            if (deathTimer <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            wasHit = true;
+            m_enemyAnimator.SetBool("isHit", true);
         }
     }
 }
--- Jeu/Assets/Scripts/Player.cs
+++ Jeu/Assets/Scripts/Player.cs
@@ -12,84 +12,148 @@ public class Player : MonoBehaviour
     private Animator m_playerAnimator = null;
     [SerializeField]
     private Rigidbody2D m_playerRigidbody = null;
+    [SerializeField]
+    private PlayerStats m_stats = null;
+    [SerializeField]
+    private GameObject m_swordRight = null;
+    [SerializeField]
+    private GameObject m_swordLeft = null;
 
     public int moveSpeed = 3;
     public int jumpHeight = 30;
+    public int pushBack = 500;
+    private float resetTimer = 0.2f;
+    private bool lookingRight = false;
+    private bool isAttacking = false;
+    private bool wasHit = false;
     private bool canJump = true;
 
     void Update()
     {
+        Attack();
         Move();
         Jump();
-        Attack();
+        ResetHit();
     }
 
     private void Move()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Attack() == false)
         {
-            m_playerTransform.position = m_playerTransform.position += (transform.right * -moveSpeed * Time.deltaTime);
-            m_playerSprite.flipX = true;
-            m_playerAnimator.SetBool("isRunning", true);
-
-            if (Input.GetKeyDown(KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.A))
             {
-                m_playerAnimator.SetBool("isSliding", true);
+                lookingRight = false;
+                m_playerTransform.position = m_playerTransform.position += (transform.right * -moveSpeed * Time.deltaTime);
+                m_playerSprite.flipX = true;
+                m_playerAnimator.SetBool("isRunning", true);
+
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    m_playerAnimator.SetBool("isSliding", true);
+                }
+                else
+                {
+                    m_playerAnimator.SetBool("isSliding", false);
+                }
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                lookingRight = true;
+                m_playerTransform.position = m_playerTransform.position += (transform.right * moveSpeed * Time.deltaTime);
+                m_playerSprite.flipX = false;
+                m_playerAnimator.SetBool("isRunning", true);
+
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    m_playerAnimator.SetBool("isSliding", true);
+                }
+                else
+                {
+                    m_playerAnimator.SetBool("isSliding", false);
+                }
             }
             else
             {
-                m_playerAnimator.SetBool("isSliding", false);
+                m_playerAnimator.SetBool("isRunning", false);
             }
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            m_playerTransform.position = m_playerTransform.position += (transform.right * moveSpeed * Time.deltaTime);
-            m_playerSprite.flipX = false;
-            m_playerAnimator.SetBool("isRunning", true);
-
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                m_playerAnimator.SetBool("isSliding", true);
-            }
-            else
-            {
-                m_playerAnimator.SetBool("isSliding", false);
-            }
-        }
-        else
-        {
-            m_playerAnimator.SetBool("isRunning", false);
         }
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        if (Attack() == false)
         {
-            m_playerRigidbody.AddForce(Vector2.up * jumpHeight);
-            m_playerAnimator.SetBool("isJumping", true);
-            canJump = false;
-        }
-        else
-        {
-            m_playerAnimator.SetBool("isJumping", false);
+            if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
+            {
+                m_playerRigidbody.AddForce(Vector2.up * jumpHeight);
+                m_playerAnimator.SetBool("isJumping", true);
+                canJump = false;
+            }
+            else
+            {
+                m_playerAnimator.SetBool("isJumping", false);
+            }
         }
     }
 
-    private void Attack()
+    private bool Attack()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
+            isAttacking = true;
+            if (lookingRight)
+            {
+                m_swordRight.SetActive(true);
+            }
+            else
+            {
+                m_swordLeft.SetActive(true);
+            }
+
             m_playerAnimator.SetBool("isAttacking", true);
+
+            return true;
         }
         else
         {
+            isAttacking = false;
+            m_swordRight.SetActive(false);
+            m_swordLeft.SetActive(false);
             m_playerAnimator.SetBool("isAttacking", false);
+
+            return false;
+        }
+    }
+
+    private void ResetHit()
+    {
+        if (wasHit)
+        {
+            resetTimer -= Time.deltaTime;
+
+            if (resetTimer <= 0)
+            {
+                wasHit = false;
+                m_playerAnimator.SetBool("isHit", false);
+                resetTimer = 0.2f;
+
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8 && isAttacking == false)
+        {
+            m_stats.Hp -= 25;
+            wasHit = true;
+            m_playerAnimator.SetBool("isHit", true);
+
+        }
     }
 }
